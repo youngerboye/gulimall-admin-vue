ef88a994fa15f62c087d3a1208d011c1c964ab80
Jackson_young <15190052893@163.com>
2020-08-07 16:28:18 +0800

    项目初始化

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        data: [],
+      }
+    },
+    created () {
+      this.getMenu()
+    },
+    methods: {
+      getMenu () {
+        this.$http({
+          url: this.$http.adornUrl('/product/pmscategory/list/tree'),
+          method: 'get',
+          params: null
+        }).then(({data}) => {
+          console.log(data)
+        })
+      }
+    }
+  }
+</script>
